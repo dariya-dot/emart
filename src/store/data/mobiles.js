abb9b11f68637540e21d301d1a0cf831cf88388c
Dariya Hussein Kondapalli <dariya.k007@gmail.com>
2024-11-27 16:12:06 +0530

    uploand and install

--- src/store/data/mobiles.js
+++ src/store/data/mobiles.js
@@ -2,7 +2,7 @@ export const mobileData = [{
 
         "id": "1",
         "product": "Mobile",
-        "image": "/assets/NewMobile/1.png",
+        "image": "/assets/Mobile/1.jpg",
         "company": "Apple",
         "model": "iPhone 13 Pro",
         "price": "1099.00",
